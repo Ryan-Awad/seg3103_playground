d581c68dfccda03117794d4fa78c8210a34a49b2
Ryan Awad <ryanawad@yahoo.com>
2024-07-03 21:50:42 -0400

    Refactor: created avg helper function

--- A2/grades/lib/grades/calculator.ex
+++ A2/grades/lib/grades/calculator.ex
@@ -1,37 +1,15 @@
 defmodule Grades.Calculator do
   def percentage_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     mark = 0.2 * avg_labs + 0.3 * avg_homework + 0.2 * midterm + 0.3 * final
     round(mark * 100)
   end
 
   def letter_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     avg_exams = (midterm + final) / 2
 
@@ -62,19 +40,8 @@ defmodule Grades.Calculator do
   end
 
   def numeric_grade(%{homework: homework, labs: labs, midterm: midterm, final: final}) do
-    avg_homework =
-      if Enum.count(homework) == 0 do
-        0
-      else
-        Enum.sum(homework) / Enum.count(homework)
-      end
-
-    avg_labs =
-      if Enum.count(labs) == 0 do
-        0
-      else
-        Enum.sum(labs) / Enum.count(labs)
-      end
+    avg_homework = avg(homework)
+    avg_labs = avg(labs)
 
     avg_exams = (midterm + final) / 2
 
@@ -101,6 +68,15 @@ defmodule Grades.Calculator do
         mark > 0.395 -> 1
         :else -> 0
       end
+    end
+  end
+
+  # Helper method for finding the average of a set of grades. If an empty set of grades is provided, the outputted average is 0.
+  def avg(grades) do
+    if Enum.count(grades) == 0 do
+      0
+    else
+      Enum.sum(grades) / Enum.count(grades)
     end
   end
 end
